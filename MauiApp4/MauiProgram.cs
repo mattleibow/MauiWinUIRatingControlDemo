591b5234de4d72bfa4a9a844f763a5e50aa05818
Matthew Leibowitz <mattleibow@live.com>
2022-08-27 23:42:25 +0200

    Add project files.

---
+++ MauiApp4/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp4;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+#if WINDOWS
+            .ConfigureMauiHandlers(handlers => handlers.AddHandler<WinUIRatingControl, WinUIRatingControlHandler>())
+#endif
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
---
+++ MauiApp4/WinUIRatingControl.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Maui.Handlers;
+
+#if WINDOWS
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#endif
+
+namespace MauiApp4;
+
+public class WinUIRatingControl : View
+{
+    public static readonly BindableProperty ValueProperty =
+        BindableProperty.Create(nameof(Value), typeof(double), typeof(WinUIRatingControl), 0.0);
+
+    public double Value
+    {
+        get => (double)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+}
+
+#if WINDOWS
+public class WinUIRatingControlHandler : ViewHandler<WinUIRatingControl, RatingControl>
+{
+    public static IPropertyMapper<WinUIRatingControl, WinUIRatingControlHandler> Mapper =
+        new PropertyMapper<WinUIRatingControl, WinUIRatingControlHandler>(ViewHandler.ViewMapper)
+        {
+            [nameof(WinUIRatingControl.Value)] = MapValue
+        };
+
+    public static CommandMapper<IPicker, IProgressBarHandler> CommandMapper =
+        new CommandMapper<IPicker, IProgressBarHandler>(ViewHandler.ViewCommandMapper)
+        {
+        };
+
+    public WinUIRatingControlHandler()
+        : base(Mapper, CommandMapper)
+    {
+    }
+
+    public WinUIRatingControlHandler(IPropertyMapper mapper, CommandMapper commandMapper = null)
+        : base(mapper ?? Mapper, commandMapper ?? CommandMapper)
+    {
+    }
+
+    protected override RatingControl CreatePlatformView() => new();
+
+    public static void MapValue(WinUIRatingControlHandler handler, WinUIRatingControl view) =>
+        handler.PlatformView.Value = view.Value;
+}
+#endif
